7c070415584d516c302e91948e66582e7a05baa6
Ivo <ibs124official@gmail.com>
2025-10-31 12:16:33 +0200

    Add js/main.injectIcons()

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,5 +3,20 @@ const CLICK = "click";
 main();
 
 function main() {
+   injectIcons();
+}
 
+function injectIcons() {
+    fetch("../icons/icons.svg")
+        .then(res => res.text())
+        .then(data => {
+            const div = document.createElement("div");
+            div.id = "icons";
+            div.style.display = "none";
+            div.innerHTML = data;
+            document.body.insertAdjacentElement("afterbegin", div);
+        })
+        .catch(err => {
+            console.error("Failed to load icons.svg:", err);
+        });
 }
