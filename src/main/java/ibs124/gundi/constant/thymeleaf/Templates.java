4831675d395f6a698fb199079e80a10ec0ee39c9
Ivo <ibs124official@gmail.com>
2025-11-09 15:38:10 +0200

    Add register success constant

--- src/main/java/ibs124/gundi/constant/thymeleaf/Templates.java
+++ src/main/java/ibs124/gundi/constant/thymeleaf/Templates.java
@@ -1,9 +1,10 @@
 package ibs124.gundi.constant.thymeleaf;
 
 public class Templates {
-    
+
     public static final String INDEX = "index";
 
     private static final String AUTH = "/auth";
     public static final String REGISTER = AUTH + "/register";
+    public static final String REGISTER_SUCCESS = REGISTER + "-success";
 }
