d01b759e1194c43abef0e6e4a415b327eea5f7ea
Ivo <ibs124official@gmail.com>
2025-11-09 14:59:08 +0200

    Move /constant/Templates:/constant/thymeleaf/Templates

---
+++ src/main/java/ibs124/gundi/constant/thymeleaf/Templates.java
@@ -0,0 +1,9 @@
+package ibs124.gundi.constant.thymeleaf;
+
+public class Templates {
+    
+    public static final String INDEX = "index";
+
+    private static final String AUTH = "/auth";
+    public static final String REGISTER = AUTH + "/register";
+}
