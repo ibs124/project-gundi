c450915cb568241f2ad56b747882ecf4efc7ee2d
Ivo <ibs124official@gmail.com>
2025-11-09 07:13:13 +0200

    Add register constatns for template and route

--- src/main/java/ibs124/gundi/constant/Routes.java
+++ src/main/java/ibs124/gundi/constant/Routes.java
@@ -1,6 +1,10 @@
 package ibs124.gundi.constant;
 
 public abstract class Routes {
-   
+
     public static final String INDEX = "/";
+
+    private static final String AUTH = "/auth";
+    public static final String REGISTER = AUTH + "/register";
+
 }
--- src/main/java/ibs124/gundi/constant/Templates.java
+++ src/main/java/ibs124/gundi/constant/Templates.java
@@ -3,4 +3,7 @@ package ibs124.gundi.constant;
 public class Templates {
     
     public static final String INDEX = "index";
+
+    private static final String AUTH = "/auth";
+    public static final String REGISTER = AUTH + "/register";
 }
