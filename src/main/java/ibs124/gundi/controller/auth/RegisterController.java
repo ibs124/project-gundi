3009fd0e76adfa058814d0486928da2445716bf2
Ivo <ibs124official@gmail.com>
2025-11-09 15:46:14 +0200

    Refactor register get mapping

--- src/main/java/ibs124/gundi/controller/auth/RegisterController.java
+++ src/main/java/ibs124/gundi/controller/auth/RegisterController.java
@@ -1,16 +1,25 @@
 package ibs124.gundi.controller.auth;
 
+import static ibs124.gundi.constant.thymeleaf.Attributes.BINDING_MODEL;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ibs124.gundi.constant.Routes;
 import ibs124.gundi.constant.Templates;
+import ibs124.gundi.model.api.RegisterBindingModel;
 
 @Controller
 public class RegisterController {
 
     @GetMapping(Routes.REGISTER)
-    public String getMethodName() {
+    public String registerGet(Model model) {
+        if (!model.containsAttribute(BINDING_MODEL)) {
+            model.addAttribute(BINDING_MODEL,
+                    new RegisterBindingModel(null, null, null, null));
+        }
+
         return Templates.REGISTER;
     }
 
