979c26d54afe26ccbae5fb7b208ead3dc9abbcd1
Ivo <ibs124official@gmail.com>
2025-11-09 15:51:45 +0200

    Add register post mapping

--- src/main/java/ibs124/gundi/controller/auth/RegisterController.java
+++ src/main/java/ibs124/gundi/controller/auth/RegisterController.java
@@ -1,19 +1,26 @@
 package ibs124.gundi.controller.auth;
 
 import static ibs124.gundi.constant.thymeleaf.Attributes.BINDING_MODEL;
+import static ibs124.gundi.constant.thymeleaf.Attributes.BINDING_RESULT;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ibs124.gundi.constant.Routes;
 import ibs124.gundi.constant.Templates;
+import ibs124.gundi.controller.AbstractController;
 import ibs124.gundi.model.api.RegisterBindingModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class RegisterController {
+@RequestMapping(Routes.REGISTER)
+public class RegisterController extends AbstractController {
 
-    @GetMapping(Routes.REGISTER)
+    @GetMapping
     public String registerGet(Model model) {
         if (!model.containsAttribute(BINDING_MODEL)) {
             model.addAttribute(BINDING_MODEL,
@@ -21,6 +28,22 @@ public class RegisterController {
         }
 
         return Templates.REGISTER;
+    }
+
+    @PostMapping
+    public String registerPost(
+            RegisterBindingModel bindingModel,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes
+                    .addFlashAttribute(BINDING_MODEL, bindingModel)
+                    .addFlashAttribute(BINDING_RESULT, bindingResult);
+            return super.redirect(Routes.REGISTER);
+        }
+
+        return super.redirect(Routes.REGISTER);
     }
 
 }
