786d1301f773b056bd7c6970f1915566a86a6285
Ivo <ibs124official@gmail.com>
2025-11-10 16:30:06 +0200

    Add model/domain/User

---
+++ src/main/java/ibs124/gundi/model/domain/User.java
@@ -0,0 +1,49 @@
+package ibs124.gundi.model.domain;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User extends AbstractAuditableDomainModel {
+
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
