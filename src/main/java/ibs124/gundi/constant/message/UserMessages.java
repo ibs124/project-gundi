d999d03871dc86fe5e8b5f929050d981d4f3de01
Ivo <ibs124official@gmail.com>
2025-11-11 12:29:18 +0200

    Add username validation

--- src/main/java/ibs124/gundi/constant/message/UserMessages.java
+++ src/main/java/ibs124/gundi/constant/message/UserMessages.java
@@ -3,11 +3,15 @@ package ibs124.gundi.constant.message;
 public abstract class UserMessages {
 
     private static final String BLANK = "must not be blank!";
-
     private static final String USERNAME = "Username ";
+
     public static final String USERNAME_BLANK = USERNAME + BLANK;
-    public static final String USERNAME_ERROR = USERNAME + """
-            must be between 3 and 20 characters long.
-            Only lowercased letters (a- z), numbers (0 - 9) and underscores (_) allowed.
-            """;
+
+    public static final String USERNAME_ERROR = """
+            Username must be between 3 and 20 characters long!
+            Allowed characters:
+                - Lowercased letters (a - z)
+                - Digits (0 - 9)
+                - Underscores (_)
+                """;
 }
