225d976b06775b71ab2c83b7dc52b5740d5b012a
Ivo <ibs124official@gmail.com>
2025-11-11 17:42:23 +0200

    Improve data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,11 @@
-INSERT INTO user_roles (id, type) VALUES
-(1, 'USER'), (2, 'ADMIN'), (3, 'ROOT');
+INSERT INTO user_roles (id,type) VALUES (1,'ROOT'), (2,'ADMIN'), (3,'USER');
 
-INSERT INTO users (id, username, email, full_name, password) VALUES
-(1, 'aegon', 'aegon.targaryen@example.com', 'Aegon Targaryen', 'rootpass'),
-(2, 'tywin', 'tywin.lannister@example.com', 'Tywin Lannister', 'adminpass'),
-(3, 'ned', 'ned.stark@example.com', 'Eddard Stark', 'adminpass'),
-(4, 'arya', 'arya.stark@example.com', 'Arya Stark', 'password'),
-(5, 'jon', 'jon.snow@example.com', 'Jon Snow', 'password'),
-(6, 'daenerys', 'daenerys.targaryen@example.com', 'Daenerys Targaryen', 'password');
+INSERT INTO users (id,username,email,full_name,password) VALUES
+(1,'aegon','aegon.targaryen@example.com','Aegon Targaryen','aaa'),
+(2,'tywin','tywin.lannister@example.com','Tywin Lannister','aaa'),
+(3,'ned','ned.stark@example.com','Eddard Stark','aaa'),
+(4,'arya','arya.stark@example.com','Arya Stark','aaa'),
+(5,'jon','jon.snow@example.com','Jon Snow','aaa'),
+(6,'daenerys','daenerys.targaryen@example.com','Daenerys Targaryen','aaa');
 
-INSERT INTO users_roles (user_id, role_id) VALUES
-(1, 3), (2, 2), (3, 2), (4, 1), (5, 1), (6, 1);
+INSERT INTO users_roles (user_id,role_id) VALUES (1,1),(2,2),(3,2),(4,3),(5,3),(6,3);
