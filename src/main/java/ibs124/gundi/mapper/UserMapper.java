b3a6cbdc16961e4e3c4405998b21a870b2d8a917
Ivo <ibs124official@gmail.com>
2025-11-12 14:26:00 +0200

    Implement model mapping

--- src/main/java/ibs124/gundi/mapper/UserMapper.java
+++ src/main/java/ibs124/gundi/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package ibs124.gundi.mapper;
 
 import ibs124.gundi.model.api.RegisterRequest;
-import ibs124.gundi.model.dto.RegisterDto;
+import ibs124.gundi.model.domain.User;
+import ibs124.gundi.model.dto.UserDto;
 
 public interface UserMapper {
 
-    RegisterDto toServiceModel(RegisterRequest src);
+    UserDto toServiceModel(User src);
+
+    User toDomainModel(RegisterRequest src);
 }
--- src/main/java/ibs124/gundi/mapper/mapstruct/UserMapstruct.java
+++ src/main/java/ibs124/gundi/mapper/mapstruct/UserMapstruct.java
@@ -1,11 +1,21 @@
 package ibs124.gundi.mapper.mapstruct;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import ibs124.gundi.config.MapStructConfig;
 import ibs124.gundi.mapper.UserMapper;
+import ibs124.gundi.mapper.UserRoleMapper;
+import ibs124.gundi.model.api.RegisterRequest;
+import ibs124.gundi.model.domain.User;
 
-@Mapper(componentModel = MapStructConfig.SPRING_COMPONENT_MODEL)
+@Mapper(componentModel = MapStructConfig.SPRING_COMPONENT_MODEL, uses = UserRoleMapper.class)
 interface UserMapstruct extends UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    User toDomainModel(RegisterRequest src);
 
 }
