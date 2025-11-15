f3698ce14c0a58a894084e6af6a54a8c2cd61401
Ivo <ibs124official@gmail.com>
2025-11-15 15:49:36 +0200

    Add validation/constraint/UniqueEmail

--- src/main/java/ibs124/gundi/model/api/RegisterRequest.java
+++ src/main/java/ibs124/gundi/model/api/RegisterRequest.java
@@ -1,11 +1,12 @@
 package ibs124.gundi.model.api;
 
+import ibs124.gundi.validation.constraint.UniqueUsername;
 import ibs124.gundi.validation.constraint.ValidEmail;
 import ibs124.gundi.validation.constraint.ValidPassword;
 import ibs124.gundi.validation.constraint.ValidUsername;
 
 public record RegisterRequest(
-        @ValidUsername String username,
+        @UniqueUsername @ValidUsername String username,
         @ValidPassword String password,
         @ValidEmail String email,
         String fullName) {
--- src/main/java/ibs124/gundi/model/dto/RegisterDto.java
+++ src/main/java/ibs124/gundi/model/dto/RegisterDto.java
@@ -1,11 +1,12 @@
 package ibs124.gundi.model.dto;
 
+import ibs124.gundi.validation.constraint.UniqueUsername;
 import ibs124.gundi.validation.constraint.ValidEmail;
 import ibs124.gundi.validation.constraint.ValidPassword;
 import ibs124.gundi.validation.constraint.ValidUsername;
 
 public record RegisterDto(
-        @ValidUsername String username,
+        @UniqueUsername @ValidUsername String username,
         @ValidPassword String password,
         @ValidEmail String email,
         String fullName) {
