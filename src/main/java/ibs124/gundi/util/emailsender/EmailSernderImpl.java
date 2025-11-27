afc470c8e3a7c89a2af685a5bcaf213826678d9c
Ivo <ibs124official@gmail.com>
2025-11-27 16:38:41 +0200

    Implement email sending method

--- src/main/java/ibs124/gundi/util/emailsender/EmailSernderImpl.java
+++ src/main/java/ibs124/gundi/util/emailsender/EmailSernderImpl.java
@@ -1,14 +1,43 @@
 package ibs124.gundi.util.emailsender;
 
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Component
 class EmailSernderImpl implements EmailSernder {
 
+    private final JavaMailSender javaMailSender;
+    private final String senderEmail;
+
+    public EmailSernderImpl(
+            JavaMailSender javaMailSender,
+            @Value("${mail_username}") String senderEmail) {
+        this.javaMailSender = javaMailSender;
+        this.senderEmail = senderEmail;
+    }
+
     @Override
     public void send(String recipient, String subject, String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'send'");
+        MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            messageHelper.setFrom(this.senderEmail, "");
+            messageHelper.setTo(recipient);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(message, true);
+
+            this.javaMailSender.send(mimeMessage);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
 }
