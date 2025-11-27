af1336fdb7a8cda29ff70f18264a9a29f8c1edcf
Ivo <ibs124official@gmail.com>
2025-11-27 16:31:21 +0200

    Add util/emailsender/EmailSenderImpl

---
+++ src/main/java/ibs124/gundi/util/emailsender/EmailSernderImpl.java
@@ -0,0 +1,14 @@
+package ibs124.gundi.util.emailsender;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class EmailSernderImpl implements EmailSernder {
+
+    @Override
+    public void send(String recipient, String subject, String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'send'");
+    }
+
+}
