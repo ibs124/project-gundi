d8d8d5598a0f3f28c62ab06ce005025220472e02
Ivo <ibs124official@gmail.com>
2025-11-28 13:55:49 +0200

    Add verification token creating service dummy impl

---
+++ src/main/java/ibs124/gundi/service/user/VerificationTokenCreatingServiceImpl.java
@@ -0,0 +1,14 @@
+package ibs124.gundi.service.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class VerificationTokenCreatingServiceImpl implements VerificationTokenCreatingService {
+
+    @Override
+    public String createByUserId(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
