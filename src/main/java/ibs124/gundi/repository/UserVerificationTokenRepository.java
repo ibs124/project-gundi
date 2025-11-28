e1be11478d0bef42f70547afd8bc0d804cfe4d0d
Ivo <ibs124official@gmail.com>
2025-11-28 13:16:43 +0200

    Add repository/UserVerificationTokenRepository

---
+++ src/main/java/ibs124/gundi/repository/UserVerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package ibs124.gundi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ibs124.gundi.model.domain.UserVerificationToken;
+
+@Repository
+public interface UserVerificationTokenRepository
+        extends JpaRepository<UserVerificationToken, Long> {
+
+}
