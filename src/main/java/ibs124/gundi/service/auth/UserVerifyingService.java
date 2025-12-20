26451b10ed44d10527a9be91a5ff1e06ddb0845b
Ivo <ibs124official@gmail.com>
2025-12-20 11:25:56 +0200

    Refactor UserVerifyingService used to return UserDTO now boolean

--- src/main/java/ibs124/gundi/service/auth/UserVerifyingService.java
+++ src/main/java/ibs124/gundi/service/auth/UserVerifyingService.java
@@ -1,8 +1,6 @@
 package ibs124.gundi.service.auth;
 
-import ibs124.gundi.model.dto.UserDTO;
-
 public interface UserVerifyingService {
-    
-    UserDTO verifyByToken(String token);
+
+    boolean verifyByToken(String token);
 }
--- src/main/java/ibs124/gundi/service/auth/UserVerifyingServiceImpl.java
+++ src/main/java/ibs124/gundi/service/auth/UserVerifyingServiceImpl.java
@@ -5,10 +5,8 @@ import java.time.Instant;
 import org.springframework.stereotype.Service;
 
 import ibs124.gundi.exception.ResourceUpdatingException;
-import ibs124.gundi.mapper.UserMapper;
 import ibs124.gundi.model.domain.User;
 import ibs124.gundi.model.domain.VerificationToken;
-import ibs124.gundi.model.dto.UserDTO;
 import ibs124.gundi.repository.UserRepository;
 import ibs124.gundi.repository.VerificationTokenRepository;
 import jakarta.validation.Validator;
@@ -18,22 +16,19 @@ class UserVerifyingServiceImpl implements UserVerifyingService {
 
     private final VerificationTokenRepository tokenRepository;
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
     private final Validator validator;
 
     public UserVerifyingServiceImpl(
             VerificationTokenRepository tokenRepository,
             UserRepository userRepository,
-            UserMapper userMapper,
             Validator validator) {
         this.tokenRepository = tokenRepository;
         this.userRepository = userRepository;
-        this.userMapper = userMapper;
         this.validator = validator;
     }
 
     @Override
-    public UserDTO verifyByToken(String request) {
+    public boolean verifyByToken(String request) {
         try {
             VerificationToken token = this.tokenRepository
                     .findByValueAndExpiresAtBefore(request, Instant.now())
@@ -48,8 +43,7 @@ class UserVerifyingServiceImpl implements UserVerifyingService {
 
             this.tokenRepository.delete(token);
 
-            return this.userMapper.toServiceModel(user);
-
+            return user.isEnabled();
         } catch (Exception e) {
             throw new ResourceUpdatingException();
         }
